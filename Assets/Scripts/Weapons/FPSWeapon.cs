9d417a39b01e3e8148c519bdef1398bd990121b0
Jeffrey Polasz <jeff_polasz@hotmail.com>
2018-05-18 18:13:26 -0400

    Setup Player's weapons

---
+++ Assets/Scripts/Weapons/FPSWeapon.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSWeapon : MonoBehaviour {
+
+    private GameObject muzzleFlash;
+
+    void Awake () {
+        muzzleFlash = transform.Find ("Muzzle Flash").gameObject;
+        muzzleFlash.SetActive (false);
+    }
+
+    public void Shoot () {
+        StartCoroutine (TurnOnMuzzleFlash ());
+    }
+
+    IEnumerator TurnOnMuzzleFlash () {
+        muzzleFlash.SetActive (true);
+        yield return new WaitForSeconds (0.1f);
+        muzzleFlash.SetActive (false);
+    }
+
+} // FPSWeapon
---
+++ Assets/Scripts/Weapons/WeaponManager.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponManager : MonoBehaviour {
+
+    public GameObject[] weapons;
+
+} // WeaponManager
