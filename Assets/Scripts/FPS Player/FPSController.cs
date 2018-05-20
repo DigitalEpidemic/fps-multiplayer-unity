92a58faadbd3eea50b8677e103ec136a869b0db2
Jeffrey Polasz <jeff_polasz@hotmail.com>
2018-05-20 19:52:26 -0400

    Fixed Main Camera problems over the network

--- Assets/Scripts/FPS Player/FPSController.cs
+++ Assets/Scripts/FPS Player/FPSController.cs
@@ -50,6 +50,11 @@ public class FPSController : NetworkBehaviour {
     private WeaponManager handsWeaponManager;
     private FPSHandsWeapon currentHandsWeapon;
 
+    public GameObject playerHolder, weaponsHolder;
+    public GameObject[] weaponsFPS;
+    private Camera mainCam;
+    public FPSMouseLook[] mouseLook;
+
     void Start () {
         // Find is not optimal
         firstPerson_View = transform.Find ("FPS View").transform;
@@ -68,9 +73,62 @@ public class FPSController : NetworkBehaviour {
 
         handsWeaponManager.weapons[0].SetActive (true);
         currentHandsWeapon = handsWeaponManager.weapons[0].GetComponent<FPSHandsWeapon> ();
+
+        if (isLocalPlayer) {
+            playerHolder.layer = LayerMask.NameToLayer ("Player");
+
+            foreach (Transform child in playerHolder.transform) {
+                child.gameObject.layer = LayerMask.NameToLayer ("Player");
+            }
+
+            for (int i = 0; i < weaponsFPS.Length; i++) {
+                weaponsFPS[i].layer = LayerMask.NameToLayer ("Player");
+            }
+
+            weaponsHolder.layer = LayerMask.NameToLayer ("Enemy");
+
+            foreach (Transform child in weaponsHolder.transform) {
+                child.gameObject.layer = LayerMask.NameToLayer ("Enemy");
+            }
+        }
+
+        if (!isLocalPlayer) {
+            playerHolder.layer = LayerMask.NameToLayer ("Enemy");
+
+            foreach (Transform child in playerHolder.transform) {
+                child.gameObject.layer = LayerMask.NameToLayer ("Enemy");
+            }
+
+            for (int i = 0; i < weaponsFPS.Length; i++) {
+                weaponsFPS[i].layer = LayerMask.NameToLayer ("Enemy");
+            }
+
+            weaponsHolder.layer = LayerMask.NameToLayer ("Player");
+
+            foreach (Transform child in weaponsHolder.transform) {
+                child.gameObject.layer = LayerMask.NameToLayer ("Player");
+            }
+        }
+
+        if (!isLocalPlayer) {
+            for (int i = 0; i < mouseLook.Length; i++) {
+                mouseLook[i].enabled = false;
+            }
+        }
+
+        // Deactivates all FPS Cameras
+        mainCam = transform.Find ("FPS View").Find ("FPS Camera").GetComponent<Camera> ();
+        mainCam.gameObject.SetActive (false);
     }
 
     void Update () {
+
+        // Activates local FPS Camera
+        if (isLocalPlayer) {
+            if (!mainCam.gameObject.activeInHierarchy) {
+                mainCam.gameObject.SetActive (true);
+            }
+        }
 
         // If you are not the local player
         // Not running on own computer
