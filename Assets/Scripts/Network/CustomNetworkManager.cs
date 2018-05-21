b1dd1ca85fceb8cdcf9097abe651f20788719f89
Jeffrey Polasz <jeff_polasz@hotmail.com>
2018-05-21 11:17:08 -0400

    Added Custom Network Manager

---
+++ Assets/Scripts/Network/CustomNetworkManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class CustomNetworkManager : NetworkManager {
+
+    private bool firstPlayerJoined;
+
+    // Only called when Auto Create Player is enabled
+    public override void OnServerAddPlayer (NetworkConnection conn, short playerControllerId) {
+        // playerPrefab is a variable from the Network Manager
+        GameObject playerObj = Instantiate (playerPrefab, Vector3.zero, Quaternion.identity);
+
+        // Does not need NetworkManager
+        List<Transform> spawnPositions = NetworkManager.singleton.startPositions;
+
+        // Stop Players from spawning in same position
+        if (!firstPlayerJoined) {
+            firstPlayerJoined = true;
+            playerObj.transform.position = spawnPositions[0].position;
+        } else {
+            playerObj.transform.position = spawnPositions[1].position;
+        }
+
+        // Add player to the server
+        NetworkServer.AddPlayerForConnection (conn, playerObj, playerControllerId);
+    }
+
+    void SetPortAndAddress () {
+        // Does not need NetworkManager
+        NetworkManager.singleton.networkAddress = "localhost";
+        NetworkManager.singleton.networkPort = 7777;
+    }
+
+    public void HostGame () {
+        SetPortAndAddress ();
+        // Does not need NetworkManager
+        NetworkManager.singleton.StartHost ();
+    }
+
+    public void JoinGame () {
+        SetPortAndAddress ();
+        // Does not need NetworkManager
+        NetworkManager.singleton.StartClient ();
+    }
+
+} // CustomNetworkManager
